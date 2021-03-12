f92e28d3e749c33eb2832084c47a9a6e7aa9e543
hyorard-b <reddream08@gmail.com>
2021-03-12 19:16:54 +0900

    [추가] 아이콘 구분

--- react/0311/component-driven/src/components/UI/Icon/Icon.js
+++ react/0311/component-driven/src/components/UI/Icon/Icon.js
@@ -1,4 +1,5 @@
-import './Icon.scss'
+import {right} from './Icon.module.scss';
+import classNames from 'classnames';
 import {ReactComponent as Email} from 'assets/Icon/email.svg'
 import {ReactComponent as Password} from 'assets/Icon/password.svg'
 import {ReactComponent as Show} from 'assets/Icon/show.svg'
@@ -6,6 +7,7 @@ import {ReactComponent as Hide} from 'assets/Icon/hide.svg'
 
 const Icon = ({type, ...restProps}) => {
   let Comp = null;
+  let pos = null;
   switch (type) {
     case 'email':
       Comp = Email;
@@ -15,15 +17,28 @@ const Icon = ({type, ...restProps}) => {
       break;
     case 'show':
       Comp = Show;
+      pos = 'right';
       break;
     case 'hide':
       Comp = Hide;
+      pos = 'right';
       break;
     default:
       break;
   }
 
-  return <Comp {...restProps} />
+
+  const classCompose = classNames(
+    pos === 'right' && right
+  );
+
+  console.log(classCompose);
+
+  return (
+    <div className={classCompose}>
+      <Comp {...restProps} />
+    </div>
+  )
 }
 
 export default Icon;
