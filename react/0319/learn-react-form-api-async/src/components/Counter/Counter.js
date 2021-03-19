e5d61c2557b28884c7c9616cff867b3c019be54c
hyorard-b <reddream08@gmail.com>
2021-03-19 11:03:00 +0900

    [추가] 통합 컴포넌트

--- react/0319/learn-react-form-api-async/src/components/Counter/Counter.js
+++ react/0319/learn-react-form-api-async/src/components/Counter/Counter.js
@@ -1,10 +1,24 @@
 // class component -> function component -> React Hooks
 
 import React from 'react';
-import CountControl from './CountControl';
-import CountDisplay from './CountDisplay';
-
+// import CountControl from './CountControl';
+// import CountDisplay from './CountDisplay';
+  
 export default class Counter extends React.Component {
+  static Display = ({ children }) => <output>{children}</output>;
+
+  static Control = ({ onUpdate, step = 1, label, children }) => {
+    const updateCount = () => {
+      if (children.includes('-')) step *= -1;
+      onUpdate(step);
+    }
+    return (
+      <button type="button" onClick={updateCount} aria-label={label}>
+        {children}
+      </button>
+    )
+  }
+
   state = {
     count: 0
   }
@@ -18,10 +32,13 @@ export default class Counter extends React.Component {
   render() {
     return (
       <div className="counterApp">
-        <CountControl label="decrement count" onUpdate={this.setCount.bind(this)}>-</CountControl>
-        <CountDisplay>{this.state.count}</CountDisplay>
-        <CountControl label="increment count" onUpdate={this.setCount.bind(this)} step={1}>+</CountControl>
+        <Counter.Control label="decrement count" onUpdate={this.setCount.bind(this)}>-</Counter.Control>
+        <Counter.Display>{this.state.count}</Counter.Display>
+        <Counter.Control label="increment count" onUpdate={this.setCount.bind(this)} step={1}>+</Counter.Control>
       </div>
     )
   }
 }
+
+Counter.Control.displayName = 'CounterControl';
+Counter.Display.displayName = 'CounterDisplay';
