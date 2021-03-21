ee4fb7f954f3889483441d2b459f0bb3251c45d1
hyorard-b <reddream08@gmail.com>
2021-03-22 00:38:29 +0900

    [추가] 검색창 및 영화 정보 렌더링

--- react/0321/search-movie/src/App.js
+++ react/0321/search-movie/src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { MoviesProvider } from './hooks/MovieContext';
+import { SearchMovie } from './components/SearchMovie';
+import { Movies } from './components/Movies';
+
 function App() {
   return (
-    
+    <MoviesProvider>
+      <SearchMovie />
+      <Movies />
+    </MoviesProvider>
   );
 }
 
