d10ca290e087d663738758a1317e8277698a5244
hyorard-b <reddream08@gmail.com>
2021-03-22 00:37:22 +0900

    [변경] 페이지 로딩 시 검색 비활성화
    영화 상세 정보 및 이미지 추가

--- react/0321/search-movie/src/components/Movies.js
+++ react/0321/search-movie/src/components/Movies.js
@@ -1,27 +1,37 @@
 import React, { useEffect } from 'react'
-import { getMovies, useMoviesDispatch, useMoviesState } from '../hooks/movieContext'
+import { GetMovies, useMoviesDispatch, useMoviesState } from './../hooks/MovieContext'
 
 export function Movies() {
   const state = useMoviesState();
   const dispatch = useMoviesDispatch();
 
   const { loading, data: movies, error } = state.movies;
-
-  useEffect(() => {
-    getMovies(dispatch);
-  });
+  console.log(movies);
 
   if (loading) return <div>로딩 중..</div>
   if (error) return <div>에러가 발생하였습니다.</div>
-  if (!movies) return <button onClick={ }>재검색하기</button>
+  if (!movies) return <button onClick={() => GetMovies(dispatch)}>재검색하기</button>
 
   return (
     <>
       <ul>
-        {movies.map(movie =>
-          <li>
-            {movie}
-          </li>)}
+        {
+          movies.map(({ title_long, summary, large_cover_image, rating, runtime }) =>
+          (
+            <li>
+              <h2>제목 : {title_long}</h2>
+              <img src={large_cover_image} alt="" width="250"/>
+              <div>
+                평점 : {rating}
+              </div>
+              <div>
+                상영 시간 : {runtime}
+              </div>
+              <p>{summary}</p>
+            </li>
+          )
+          )
+        }
       </ul>
     </>
   )
