a92885d87cc5be3906390fd4670a42908b4c4ab7
hyorard-b <reddream08@gmail.com>
2021-03-25 16:13:49 +0900

    [추가] 북마크 컴포넌트 내보내기

--- react/0325/my-movies/src/components/index.js
+++ react/0325/my-movies/src/components/index.js
@@ -3,3 +3,4 @@ export { default as Profile } from './Profile/Profile'
 export { default as Navigation } from './Navigation/Navigation'
 export { default as Effects } from './Effects/Effects'
 export { default as YoutubePlayer } from './YoutubePlayer/YoutubePlayer'
+export { default as BookmarkButton } from './Bookmark/Bookmark';
